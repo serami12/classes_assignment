4ab2a233cf1ecd9896a54a6aab11ebfce4d14b14
serami12 <stephanieramirez1@pursuit.org>
2019-11-22 18:51:58 -0500

    added the word new Book to the instance

--- classes.js
+++ classes.js
@@ -7,21 +7,21 @@ a. Write a class called Person that has 3 properties: a first name, a last name 
  b. Write a method in Person called fullName that will
  return a formatted string of an instance 's full name. Call this method on both the instances you created in part a.*/
 
-class Person {
-    constructor(firstName, middleName, lastName) {
-        this.firstName = firstName;
-        this.middleName = middleName;
-        this.lastName = lastName;
-    }
-    fullName() {
-        return `${this.firstName} full name is ${this.firstName} ${this.middleName} ${this.lastName}.`
-    }
-}
-const personOne = new Person("Beyonce", "Giselle", "knowles-Carter")
-const personTwo = new Person("Shawn", "Corey", "Carter")
+// class Person {
+//     constructor(firstName, middleName, lastName) {
+//         this.firstName = firstName;
+//         this.middleName = middleName;
+//         this.lastName = lastName;
+//     }
+//     fullName() {
+//         return `${this.firstName} full name is ${this.firstName} ${this.middleName} ${this.lastName}.`
+//     }
+// }
+// const personOne = new Person("Beyonce", "Giselle", "knowles-Carter")
+// const personTwo = new Person("Shawn", "Corey", "Carter")
 
-console.log(personOne.fullName())
-console.log(personTwo.fullName())
+// console.log(personOne.fullName())
+// console.log(personTwo.fullName())
 
 /*
 Question 2
@@ -30,11 +30,11 @@ a.Create a class called Book that has properties title, author and rating.Create
 b.b.Add a method to Book called isGood that returns true
 if its rating is greater than or equal to 7. */
 
-class Books {
+class Book {
     constructor(title, author, rating) {
         this.title = title;
         this.author = author;
         this.rating = rating;
     }
 }
-let bookOne = ("Javascript for Kids", "Nick Morgan", 10)
+let bookOne = new Book("Javascript for Kids", "Nick Morgan", 10);
