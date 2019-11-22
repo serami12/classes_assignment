ad5d679229069b49054926f55ce3e7ed807483cc
serami12 <stephanieramirez1@pursuit.org>
2019-11-22 18:19:50 -0500

    answered q2 part A

--- classes.js
+++ classes.js
@@ -1,7 +1,11 @@
 // #
 // Class Exercises
 /* 
+Question 1
 a. Write a class called Person that has 3 properties: a first name, a last name and a middle name. Create 2 instances of a Person. Print one of their first names.*/
+/*
+ b. Write a method in Person called fullName that will
+ return a formatted string of an instance 's full name. Call this method on both the instances you created in part a.*/
 
 class Person {
     constructor(firstName, middleName, lastName) {
@@ -9,12 +13,28 @@ class Person {
         this.middleName = middleName;
         this.lastName = lastName;
     }
+    fullName() {
+        return `${this.firstName} full name is ${this.firstName} ${this.middleName} ${this.lastName}.`
+    }
 }
-let personOne = new Person("Beyonce", "Giselle", "knowles-Carter")
-let personTwo = new Person("Shawn", "Corey", "Carter")
+const personOne = new Person("Beyonce", "Giselle", "knowles-Carter")
+const personTwo = new Person("Shawn", "Corey", "Carter")
 
-console.log(personOne.firstName)
+console.log(personOne.fullName())
+console.log(personTwo.fullName())
 
 /*
-b. Write a method in Person called fullName that will
-return a formatted string of an instance 's full name. Call this method on both the instances you created in part a.*/
+Question 2
+a.Create a class called Book that has properties title, author and rating.Create some instances of Book. */
+/* 
+b.b.Add a method to Book called isGood that returns true
+if its rating is greater than or equal to 7. */
+
+class Books {
+    constructor(title, author, rating) {
+        this.title = title;
+        this.author = author;
+        this.rating = rating;
+    }
+}
+let bookOne = ("Javascript for Kids", "Nick Morgan", 10)
